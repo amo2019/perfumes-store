eef858f3e12d26e0e6ed4596493fe4d08ebfb153
amo2019 <aminvpython@gmail.com>
2022-01-12 14:28:46 +0100

    add PayPal payment method

--- client/src/components/CheckoutComponent/CheckoutComponent.tsx
+++ client/src/components/CheckoutComponent/CheckoutComponent.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import CheckoutItem from '../CheckoutItem/CheckoutItem';
 import { CartItem, clearCart } from "../../lib/cart";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import Transaction from '../Modal/Transaction';
 import './checkoutStyles.css';
-
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
 const CheckoutPage = ({...cartItems}: CartItem[]) => {
+  const [open, setOpen] = useState(false);
+  const [paid, setPaid] = useState(false);
+  const currency = "USD";
+  const style: any = { layout: "vertical" };
+  const dispatch = useDispatch();
+  
   const propertyValues = Object.values(cartItems);
 
   const resultArray = Object.keys(cartItems).map((personNamedIndex:any) => cartItems[personNamedIndex]);
@@ -15,9 +26,66 @@ const CheckoutPage = ({...cartItems}: CartItem[]) => {
     0
   ) 
 
+  let prevTotal = total;
+  const secretKey: any = process.env.REACT_APP_SECRET_CODE?.toString();
+
 const handleClick = ()=>{
   clearCart("1")
 }
+
+const handlePaid = ()=>{
+  setPaid(true);
+  //setTimeout(()=>{setPaid(false);}, 6000)
+}
+  const ButtonWrapper = ({ currency, showSpinner }:any) => {
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, []);
+
+    return (
+      <>
+        <div className="spinner" />
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[total, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: total.toString(),
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              handlePaid();
+              handleClick();
+            });
+          }}
+        />
+      </>
+    );
+  };
+
   return (
   
   <div className='checkoutPageContainer'>
@@ -44,11 +112,29 @@ const handleClick = ()=>{
     )}
     <div className='flex-div'>
     <div className='totalContainer'>TOTAL: ${total}</div>
+    {open && total > 0 ? (
+        <div className="paymentMethods">
+              <PayPalScriptProvider
+                options={{
+                  "client-id": secretKey,
+                    components: "buttons",
+                    currency: "USD",
+                    "disable-funding": "sepa,sofort,card,p24,giropay",
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
+            </div>
+       ):( <button onClick={() => setOpen(true)} className="button">
+              CHECKOUT NOW!
+            </button>
+       )}
     <button className="clearButton" onClick={handleClick} >
               Clear Cart
     </button>
     </div>
     <div className='warningContainer'>
+    {paid && <Transaction total={prevTotal} setPaid={setPaid}/>}
       Please use one of the following test credit cards
       <br />
       4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
@@ -57,5 +143,7 @@ const handleClick = ()=>{
     </div>
   </div>
 );
-    }
+
+}
+
 export default CheckoutPage;
---
+++ client/src/components/Modal/Transaction.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./transaction.css";
+
+const Transaction = ({ total, setPaid }) => {
+  const [customer, setCustomer] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleClick = ()=>{
+    setPaid(false)
+  }
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h1 className="title">You have paid {total} successfully.</h1>
+        <div className="item">
+        </div>
+        <button className="modal-button" onClick={handleClick}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Transaction;
